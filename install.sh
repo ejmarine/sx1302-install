343c630aa77654117f2992450eb1da34cdaabdc1
Teeddy-LB <83368661+ejmarine@users.noreply.github.com>
2025-08-01 17:52:51 -0400

    Misc fixes

--- install.sh
+++ install.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+
+if [ "$EUID" -eq 0 ]; then
+    echo "NOTE: don't run me as root"
+    exit
+fi
+
 sudo apt update
 sudo apt install git
 
-git clone https://github.com/Lora-net/sx1302_hal.git ~/Documents
+git clone https://github.com/Lora-net/sx1302_hal.git ~/Documents/sx1302_hal
+
+cp install_svc.sh ~/Documents/sx1302_hal/
 
 read -p "Apply fix for Pi3B? (y/n)" fix
 
@@ -14,7 +22,7 @@ fi
 read -p "Add global_conf.json (this only exists because I was having issues with the default one and the frequencies it uses. This one runs on FSB1 rather than FSB2)? (y/n)" add_conf
 
 if [ "$add_conf" == "y" ]; then
-    cp global_conf.json ~/Documents/sx1302_hal/
+    cp global_conf.json ~/Documents/sx1302_hal/packet_forwarder/
 fi
 
 cd ~/Documents/sx1302_hal/
@@ -28,4 +36,4 @@ cp tools/reset_lgw.sh packet_forwarder/
 echo "Run 'chip_id' in util_chip_id/ to get concentrator EUI and add it to Chirpstack"
 echo "Then, add that EUI and your Chirpstack server address to global_conf.json"
 echo "After that, run 'lora_pkt_fwd' in packet_forwarder/ to make sure it works."
-echo "Finally, run 'install_svc.sh' to run it as a service."
+echo "Finally, run 'sudo bash install_svc.sh' located in ~/Documents/sx1302_hal/ to run it as a service."
--- install_svc.sh
+++ install_svc.sh
@@ -2,9 +2,8 @@
 
 echo "Installing as a service..."
 
-cd ~/Documents/sx1302_hal/
-
-cat > /etc/systemd/system/lora-pkt-fwd.service <<EOF
+sudo touch /etc/systemd/system/lora-pkt-fwd.service
+sudo cat > /etc/systemd/system/lora-pkt-fwd.service <<EOF
 [Unit]
 Description=LoRa Packet Forwarder
 After=network.target
@@ -12,8 +11,8 @@ After=network.target
 [Service]
 User=$USER
 Group=www-data
-WorkingDirectory=$(pwd)
-ExecStart=$(pwd)/packet_forwarder/lora_pkt_fwd
+WorkingDirectory=$(pwd)/packet_forwarder
+ExecStart=$(pwd)/packet_forwarder/lora_pkt_fwd -c /home/pi/Documents/sx1302_hal/packet_forwarder/global_conf.json
 ExecReload=/bin/kill -s HUP $MAINPID
 Restart=on-failure
 
@@ -24,5 +23,7 @@ EOF
 sudo systemctl daemon-reload
 sudo systemctl enable lora-pkt-fwd
 sudo systemctl start lora-pkt-fwd
+
+sudo systemctl status lora-pkt-fwd
 
 echo "Done!"
